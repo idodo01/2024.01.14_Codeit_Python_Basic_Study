56c6d743e492a52a234fc73f4826d8df1565578e
IDO <susususu07@naver.com>
2024-01-19 19:40:10 +0900

    Add files via upload

---
+++ functionTest1.py
@@ -0,0 +1,7 @@
+def hello():
+    print("Hello")
+    print("Welcome to Codeit!")
+    
+print("함수 호출 전")
+hello()
+print("함수 호출 후")
---
+++ functionTest2.py
@@ -0,0 +1,6 @@
+def square(x):
+    return x * x
+    
+print("함수 호출 전")
+print(square(3) * square(4))
+print("함수 호출 후")
